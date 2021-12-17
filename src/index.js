faa6ab64b65ee40f33808e8c4f7f41573ea69171
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-12-17 15:43:16 -0700

    enabled hot reloading, css, and added some scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import './styles/index.css';
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(process.env.MAIN_URL);
 web3.eth
   .getBlockNumber()
   .then((blockNumber) => console.log("Block number: " + blockNumber));
+
+
+const objectx = {
+  l: 1
+}
+console.log(objectx);
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,43 @@ const path = require("path");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { loader } = require("mini-css-extract-plugin");
 
 plugins = [
   new NodePolyfillPlugin(),
   new Dotenv(),
-  
-],
+  new MiniCssExtractPlugin(),
+];
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
 module.exports = {
-  mode: 'development',
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+  mode: mode,
+  // entry: "./src/index.js",
+  // output: {
+  //   //filename: "main.js",
+  //   path: path.resolve(__dirname, "dist"),
+  // },
   devtool: 'source-map',
   devServer: {
-    static: './dist'
+    static: './dist',
+    hot: true
   },
 
   module: {
     rules: [
+      {   // supports sass aswell which im not using
+        test: /\.(c[ac]|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader', 
+          'postcss-loader',
+        ], 
+      },
       {
         test: /\.js?$/,
         exclude: /node_modules/,
